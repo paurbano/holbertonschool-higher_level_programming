eb172bde2ccc08b7fbf98f48cd9b3d61fb2e9ed6
paurbano <paurbano@gmail.com>
2019-12-11 14:33:51 +0000

    fix data type in malloc

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -11,7 +11,7 @@ listint_t *insert_node(listint_t **head, int number)
 {
 	listint_t *newNode = NULL, *prevNode = NULL, *currentNode = NULL;
 
-	newNode = malloc(sizeof(int));
+	newNode = malloc(sizeof(listint_t));
 
 	if (newNode == NULL)
 		return (NULL);

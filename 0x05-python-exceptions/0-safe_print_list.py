5b1af528ab9c5a12dc2694bf5d0a78473144f329
paurbano <paurbano@gmail.com>
2020-01-08 06:18:12 +0000

    from a list

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    cont = 0
+    for x in my_list:
+        print("{:d}".format(i),end="")
+        print("\n")
+        cont= cont + 1
+    return cont

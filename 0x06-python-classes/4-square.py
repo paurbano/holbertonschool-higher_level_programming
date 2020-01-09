cac0f9a55b3accc6f6c5c81cbb96af7be459b80f
paurbano <paurbano@gmail.com>
2020-01-09 17:48:46 +0000

    class Square that defines a square by: (based on 3-square.py) added property setter

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,32 @@
+class Square:
+    """class Square that defines a square.
+
+    Attributes:
+              __size (int): size of side of square
+    """
+    __size = None
+
+    def __init__(self, size=0):
+        """__init__ method.
+        Args:
+            size (int): size of square
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """area return current square area"""
+        return self.__size * self.__size

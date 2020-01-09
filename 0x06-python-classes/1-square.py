df44907818dac86a9c4399ac79ce0688aa136fa2
paurbano <paurbano@gmail.com>
2020-01-09 20:33:30 +0000

    class Square that defines a square by: (based on 0-square.py) added attribute size

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 class Square:
     """empty class Square that defines a square."""
-    __size = None
-
-    def __init__(self,size):
+    def __init__(self, size):
         """__init__ method.
         Args:
             size (int): size of square

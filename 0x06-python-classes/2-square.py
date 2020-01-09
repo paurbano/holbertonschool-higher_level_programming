c5b58a928394aa6e5cb19cd9a45816cb1bf6f1d3
paurbano <paurbano@gmail.com>
2020-01-09 17:45:55 +0000

    class Square with private attribute and init method

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,19 @@
+class Square:
+    """class Square that defines a square.
+
+    Attributes:
+              __size (int): size of side of square
+    """
+    __size = None
+
+    def __init__(self, size=0):
+        """__init__ method.
+        Args:
+            size (int): size of square
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size

450667694d2efff6611fda2a57b1671766503b71
pablo <araneumit@gmail.com>
2020-01-17 23:19:05 -0500

    Fix task2

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
+""" Module Say_my_name"""
+
+
 def say_my_name(first_name, last_name=""):
+    """  prints My name is <first name> <last name> """
     if type(first_name) is not str:
         raise TypeError("first_name must be a string")
     if type(last_name) is not str:
         raise TypeError("last_name must be a string")
 
-    if first_name == "" and last_name == "":
-        return
     name = "My name is " + first_name.strip() + " " + last_name.strip()
     print(name.strip())

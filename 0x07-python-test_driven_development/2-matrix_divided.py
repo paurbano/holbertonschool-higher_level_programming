0db643c9190f79489f9bf6a626cd2650270b9f78
pablo <araneumit@gmail.com>
2020-01-17 22:53:22 -0500

    function that divides all elements of a matrix.

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,17 +1,37 @@
 #!/usr/bin/python3
-def matrix_divided(matrix, div):
-    msg = "matrix must be a matrix (list of lists) of integers/floats"
-    if type(matrix) not in (int, float):
-        raise TypeError(msg)
+"""2-matrix_divided  module"""
 
-    for row in matrix:
-        if len(row) == len(matrix[0]):
-            pass
-        else:
-            raise TypeError("Each row of the matrix must have the same size")
+
+def matrix_divided(matrix, div):
+    "function that divides all elements of a matrix."
+
+    new_matrix = []
+    msg = "matrix must be a matrix (list of lists) of integers/floats"
 
     if type(div) not in (int, float):
         raise TypeError("div must be a number")
 
     if div == 0:
         raise ZeroDivisionError("division by zero")
+
+    for row in matrix:
+        if len(row) != len(matrix[0]):
+            raise TypeError("Each row of the matrix must have the same size")
+            for num in row:
+                if type(num) not in (int, float):
+                    raise TypeError(msg)
+
+    for row in matrix:
+        if len(row) == len(matrix[0]):
+            tmp_lista = []
+            for num in row:
+                if type(num) not in (int, float):
+                    raise TypeError(msg)
+                else:
+                    tmp_lista.append(round(num / div, 2))
+        else:
+            raise TypeError("Each row of the matrix must have the same size")
+
+        new_matrix.append(tmp_lista)
+
+    return new_matrix

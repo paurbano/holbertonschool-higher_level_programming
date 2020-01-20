89ddf18794b03a18399894695e468151057499f6
pablo <araneumit@gmail.com>
2020-01-20 16:08:49 -0500

    prints a square with the character #.

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -4,13 +4,13 @@
 
 def print_square(size):
     """ print a square with the character # """
-    if size < 0:
-        raise ValueError("size must be >= 0")
-
     if type(size) is not int:
         raise TypeError("size must be an integer")
 
-    if type(size) == float and size < 0:
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    if type(size) is float and size < 0:
         raise TypeError("size must be an integer")
 
     for i in range(size):

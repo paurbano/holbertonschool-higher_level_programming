59be5174acaf89080ef3022dbfbf4676426d3541
pablo <araneumit@gmail.com>
2020-01-20 18:10:22 -0500

    change condition

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def add_integer(a, b=98):
-    if type(a) not in (int, float):
+    if not isinstance(a, (int, float)):
         raise TypeError('a must be an integer')
 
-    if type(b) not in (int, float):
+    if not isinstance(b, (int, float)):
         raise TypeError('b must be an integer')
 
     if type(a) == float:

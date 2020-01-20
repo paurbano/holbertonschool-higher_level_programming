df3f36e0c2388c987129a86019217e70033928bf
pablo <araneumit@gmail.com>
2020-01-20 18:02:46 -0500

    Unittest

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -5,25 +5,24 @@ max_integer = __import__('6-max_integer').max_integer
 
 
 class TestMaxInteger(unittest.TestCase):
-
     """ tests for max_integer """
 
-    def test_max_int_basic(self):
+    def test_max(self):
         """ tests normal list of ints
         """
         self.assertEqual(max_integer([1, 2, 3, 4]), 4)
 
-    def test_max_int_empty(self):
+    def test_max_empty(self):
         """ tests if list is empty
         """
         self.assertEqual(max_integer([]), None)
 
-    def test_max_int_neg(self):
+    def test_max_integer_neg(self):
         """ tests if list has a negative int
         """
         self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
 
-    def test_max_int_one(self):
+    def test_max_integer_one(self):
         """ tests if list has only one item
         """
         self.assertEqual(max_integer([1]), 1)

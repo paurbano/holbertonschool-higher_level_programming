263c1652a798a34b039411737d5091931823ae4b
pablo <araneumit@gmail.com>
2020-01-21 17:56:59 -0500

    appends a string at the end of a text file (UTF8) and returns the number of characters added:

---
+++ 0x0B-python-input_output/4-append_write.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" module appends a string at the end of a text file (UTF8)
+and returns the number of characters written:
+"""
+
+
+def append_write(filename="", text=""):
+    """ appends a string at the end of a text file"""
+    with open(filename, 'a+') as f:
+        num_chac = f.write(text)
+
+    return num_chac

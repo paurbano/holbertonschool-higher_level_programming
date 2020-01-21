304635b3497da1aee3b637a6363c579e9288287a
pablo <araneumit@gmail.com>
2020-01-21 00:17:33 -0500

    class BaseGeometry (based on 5-base_geometry.py).

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Class baseGeometry Module
+"""
+
+
+class BaseGeometry:
+    """
+    A class geometry
+    """
+    def area(self):
+        """
+        Returns the area of an descendent
+        """
+        raise Exception("area() is not implemented")

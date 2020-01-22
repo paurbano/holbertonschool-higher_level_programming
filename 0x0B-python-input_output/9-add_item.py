f50038459935dbfb32a5b8dd4e37f2bf4ac6da3b
pablo <araneumit@gmail.com>
2020-01-21 22:30:44 -0500

    adds all arguments to a Python list, and then save them to a file:

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,20 +1,19 @@
 #!/usr/bin/python3
-"""Load, add, save """
+"""Module for task9."""
 
 
 from sys import argv
 
-load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+save_file = __import__('7-save_to_json_file').save_to_json_file
+load_file = __import__('8-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
 
 try:
-    lista = load_from_json_file(filename)
+    my_list = load_file(filename)
 except FileNotFoundError:
-    lista = []
+    my_list = []
 
-for n in range(len(argv)):
-    if n != 0:
-        lista.append(argv[n])
-save_to_json_file(lista, filename)
+for n in range(1, len(argv)):
+    my_list.append(argv[n])
+save_file(my_list, filename)

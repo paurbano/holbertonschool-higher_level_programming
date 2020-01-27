38076991eaa9c08707f64050f0ced36996f2298d
paurbano <55122159+paurbano@users.noreply.github.com>
2020-01-27 17:49:02 -0500

    fix text validation

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -38,7 +38,7 @@ class Rectangle(Base):
         if type(height) != int:
             raise TypeError("height must be an integer")
         elif height < 0:
-            raise ValueError("height must be >= 0")
+            raise ValueError("height must be > 0")
         else:
             self.__height = height
 
@@ -53,7 +53,7 @@ class Rectangle(Base):
         if type(width) != int:
             raise TypeError("width must be an integer")
         elif width < 0:
-            raise ValueError("width must be >= 0")
+            raise ValueError("width must be > 0")
         else:
             self.__width = width
 
--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -31,7 +31,7 @@ class Square(Rectangle):
         if type(value) != int:
             raise TypeError("width must be an integer")
         elif value < 0:
-            raise ValueError("width must be >= 0")
+            raise ValueError("width must be > 0")
         else:
             self.width = value
             self.height = value

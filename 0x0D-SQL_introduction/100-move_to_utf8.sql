b7e6569c6ec726706893c5e3876820dbe88ea5d0
paurbano <paurbano@gmail.com>
2020-03-02 20:21:45 +0000

    converts hbtn_0c_0 database to UTF8

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,5 @@
 -- converts hbtn_0c_0 database to UTF8 (utf8mb4, collate utf8mb4_unicode_ci) 
+-- converts hbtn_0c_0 database to UTF8
 USE hbtn_0c_0;
 
 ALTER DATABASE hbtn_0c_0
@@ -8,5 +9,4 @@ ALTER TABLE first_table
 CONVERT TO CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
 
-ALTER TABLE first_table
-MODIFY name COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table MODIFY name VARCHAR(256) COLLATE utf8mb4_unicode_ci;

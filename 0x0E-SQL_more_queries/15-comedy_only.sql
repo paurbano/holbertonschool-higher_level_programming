a70e8ce717bd2466d30945b316e13bd1783296d3
paurbano <paurbano@gmail.com>
2020-03-04 00:21:31 +0000

    lists all Comedy shows

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,8 @@
+-- lists all Comedy shows in the database hbtn_0d_tvshows.
+-- with genre
+SELECT DISTINCT tv_shows.title AS name
+FROM tv_shows
+INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+INNER JOIN tv_genres  ON tv_genres.id = tv_show_genres.genre_id
+WHERE tv_genres.name = 'Comedy'
+ORDER BY 1 ASC;

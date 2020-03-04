9af6a65e470ebee17306c2f50ae9e5ea5629ff1c
paurbano <paurbano@gmail.com>
2020-03-04 00:53:55 +0000

    creates the database hbtn_0d_usa and the table states

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,6 +1,7 @@
 -- creates the db hbtn_0d_usa
 -- creates the table states
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(256) NOT NULL,

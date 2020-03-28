f8df81a0d05b34deea9a140adf77deab33dd9030
paurbano <paurbano@gmail.com>
2020-03-28 09:10:54 +0000

    same 2 but prevent sql injection

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+Module States
+script that lists all states with a name starting with N (upper N)
+"""
+from sys import argv
+import MySQLdb
+if __name__ == '__main__':
+    MY_USER = argv[1]
+    MY_PASSWD = argv[2]
+    MY_DB = argv[3]
+    param = str(argv[4])
+
+    '''Open Database connection'''
+    mydb = MySQLdb.connect(host="localhost",
+                           user=MY_USER,
+                           passwd=MY_PASSWD,
+                           db=MY_DB)
+
+    '''Create cursor for operate over DB'''
+    myCursor = mydb.cursor()
+
+    '''pass and execute a SQL sentence'''
+    '''prevents sql Injection'''
+    sql = "select * from states where name = %s"
+    myCursor.execute(sql,(param,))
+
+    '''retrive records and fill cursor'''
+    states = myCursor.fetchall()
+
+    '''Print rows'''
+    for state in states:
+        print(state)
+
+    '''close cursor'''
+    myCursor.close()
+
+    '''close connection to DB'''
+    mydb.close()

c17c80453cdd202b8fcfa202bd34f37dae94be70
paurbano <paurbano@gmail.com>
2020-03-28 18:14:30 +0000

    adds the State object “Louisiana” to the database

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+''' adds the State object “Louisiana” to the database hbtn_0e_6_usa '''
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    dbConnector = 'mysql+mysqldb://{}:{}@localhost/{}'
+    engine = create_engine(dbConnector.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Base.metadata.bind = engine
+    DBSession = sessionmaker()
+    DBSession.bind = engine
+    session = DBSession()
+
+    # create state object
+    addState = State("Louisiana")
+    # commit and close session
+    session.add(addState)
+    session.commit()
+
+    query = session.query(State).filter(
+        State.name == 'Louisiana')
+
+    if query.count() > 0:
+        for instance in query:
+            print(str(instance.id))
+
+    session.close()

b5ef4fb3e378b13b32258a0cf8f21fdff2592635
paurbano <paurbano@gmail.com>
2020-03-28 17:30:40 +0000

    lists all states from the database hbtn_0e_0_usa

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+Module States
+script that lists all states from the database hbtn_0e_0_usa
+"""
+from sys import argv
+import MySQLdb
+if __name__ == '__main__':
+    MY_USER = argv[1]
+    MY_PASSWD = argv[2]
+    MY_DB = argv[3]
+
+    '''Open Database connection'''
+    mydb = MySQLdb.connect(host="localhost",
+                           user=MY_USER,
+                           passwd=MY_PASSWD,
+                           db=MY_DB)
+
+    '''Create cursor for operate over DB'''
+    myCursor = mydb.cursor()
+
+    '''pass and execute a SQL sentence'''
+    myCursor.execute("select * from states \
+                     order by states.id")
+
+    '''retrive records and fill cursor'''
+    states = myCursor.fetchall()
+
+    '''Print rows'''
+    for state in states:
+        print(state)
+
+    '''close cursor'''
+    myCursor.close()
+
+    '''close connection to DB'''
+    mydb.close()

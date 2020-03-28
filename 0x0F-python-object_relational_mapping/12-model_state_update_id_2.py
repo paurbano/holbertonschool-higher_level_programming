0a5ae87a8ca064c4396debe6148e090261f315f5
paurbano <paurbano@gmail.com>
2020-03-28 20:46:10 +0000

    task 12

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -5,22 +5,22 @@ import sys
 from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.sql.expression import update
 
 if __name__ == "__main__":
-    dbConnector = 'mysql+mysqldb://{}:{}@localhost/{}'
+    dbConnector = 'mysql+mysqldb://{}:{}@localhost:3306/{}'
     engine = create_engine(dbConnector.format(
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
     Base.metadata.bind = engine
-    DBSession = sessionmaker()
-    DBSession.bind = engine
+    DBSession = sessionmaker(bind=engine)
     session = DBSession()
 
-    # create state object
-    state = session.query(State).filter_by(
-            id=2)
-
+    # create and query state object
+    state = session.query(State).filter_by(id=2)
+    # state = State.update().where(State.name == 'New Mexico').values(id=2)
     state.name = 'New Mexico'
     # commit and close session
+    session.commit()
     session.commit()
     session.close()

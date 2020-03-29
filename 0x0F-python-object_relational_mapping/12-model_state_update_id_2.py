bc9767a0f0402004bfa0bb4e868c1f025e4d8187
paurbano <paurbano@gmail.com>
2020-03-29 03:11:15 +0000

    changes the name of a State object

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -7,19 +7,16 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    # dbConnector = 'mysql+mysqldb://{}:{}@localhost:3306/{}'
     engine = create_engine(
         'mysql+mysqldb://{}:{}@localhost:3306/{}'.
         format(sys.argv[1], sys.argv[2], sys.argv[3]),
         pool_pre_ping=True)
     Base.metadata.create_all(engine)
-    Base.metadata.bind = engine
-    DBSession = sessionmaker(bind=engine)
-    session = DBSession()
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
     # create and query state object
-    state = session.query(State).filter_by(id=2)
-    # state = State.update().where(State.name == 'New Mexico').values(id=2)
+    state = session.query(State).filter_by(id=2).first()
     state.name = 'New Mexico'
     # commit and close session
     session.commit()

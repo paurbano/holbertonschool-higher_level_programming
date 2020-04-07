336cdf4af9d2485e9bcb74f57a8602c85d71e60d
paurbano <paurbano@gmail.com>
2020-04-07 03:42:50 +0000

    sends a request to that URL, and displays the size of the body of the response

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a request to that URL, and displays the size of the body of the response
-curl -sSI $1 |grep -Fi 'Content-Length' | rev | cut -d ' ' -f1 | rev
+curl -sSI "$1" |grep -Fi 'Content-Length' | rev | cut -d ' ' -f1 | rev

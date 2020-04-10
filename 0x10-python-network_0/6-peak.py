721bd9ad26efb7ad6dc6b188a09e4c1e1711a1e7
paurbano <paurbano@gmail.com>
2020-04-10 22:57:14 +0000

    find peak in a list

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -9,14 +9,29 @@ def find_peak(list_of_integers):
         return list_of_integers[0]
     if len(list_of_integers) == 2:
         return max(list_of_integers)
-    return peak(list_of_integers, 0, len(list_of_integers)-1)
 
+    _list = list_of_integers
+    mid = int(len(_list) / 2)
+    length = len(_list)
+    if (_list[mid - 1] <= _list[mid]) and (_list[mid + 1] <= _list[mid]):
+        return _list[mid]
+    elif _list[mid] < _list[mid + 1]:
+        return find_peak(_list[mid + 1:])
+        # return peak(array, mid + 1, high)
+    else:
+        # return peak(array, low, mid - 1)
+        return find_peak(_list[:mid - 1])
+    # return peak(list_of_integers, 0, len(list_of_integers) - 1)
 
+# not used
+'''
 def peak(array, low, high):
     mid = int((low + high) / 2)
+    l = len(array)
     if (array[mid - 1] <= array[mid]) and (array[mid + 1] <= array[mid]):
         return array[mid]
     elif array[mid] < array[mid + 1]:
         return peak(array, mid + 1, high)
     else:
         return peak(array, low, mid - 1)
+'''

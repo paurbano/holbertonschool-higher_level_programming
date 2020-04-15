016a175ffa3e0164eb6f3d24d0fbda43917838de
paurbano <paurbano@gmail.com>
2020-04-15 20:29:55 +0000

    fetches URL with urllib

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     import urllib.request
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         answer = response.read()
-        print("Body Response:")
+        print("Body response:")
         print("    - type: {}".format(type(answer)))
         print("    - content: {}".format(answer))
         print("    - utf8 content: {}".format(answer.decode('utf-8')))

26924cc96cfb8b56a6ed9e16e1fe4f479ff92296
paurbano <paurbano@gmail.com>
2020-04-15 23:29:12 +0000

    use the package requests

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -8,5 +8,5 @@ if __name__ == "__main__":
         r = requests.get('https://intranet.hbtn.io/status')
         # print(r.text)
         print("Body Response:")
-        print("    - type: {}".format(type(r.text)))
-        print("    - content: {}".format(r.text))
+        print("\t- type: {}".format(type(r.text)))
+        print("\t- content: {}".format(r.text))

5bad6172644f57c57eb20910460ccede3b7b1b9d
paurbano <paurbano@gmail.com>
2020-05-06 04:01:34 +0000

    fix semistandard

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,7 +4,7 @@ if (process.argv.length === 2 || process.argv.length === 3) {
   console.log(0);
 } else {
   array = process.argv.slice(2);
-  array.sort(function(a, b){return a - b});
+  array.sort(function (a, b) { return a - b; });
   array.reverse();
   console.log(array[1]);
 }

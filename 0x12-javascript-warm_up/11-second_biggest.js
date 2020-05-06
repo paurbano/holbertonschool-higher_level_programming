1940a8d9e959c03bbf3a28caea78bc09c2b66f8d
paurbano <paurbano@gmail.com>
2020-05-06 03:49:47 +0000

    change print 0 to '0'

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 let array;
 if (process.argv.length === 2 || process.argv.length === 3) {
-  console.log(0);
+  console.log('0');
 } else {
   array = process.argv.slice(2);
   array.sort();

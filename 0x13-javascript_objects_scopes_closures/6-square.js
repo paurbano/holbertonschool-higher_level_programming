56759adfe642663824a53dd7392321ccf7609a80
paurbano <paurbano@gmail.com>
2020-05-11 23:52:25 +0000

    fix call argument

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
-const SquareFather = require('./5-square');
+const Father = require('./5-square');
 
-module.exports = class Square extends SquareFather {
+module.exports = class Square extends Father {
   charPrint (c) {
     if (c === undefined) {
       this.print();
     } else {
       for (let i = 0; i < this.height; i++) {
-        console.log('C'.repeat(this.width));
+        console.log(c.repeat(this.width));
       }
     }
   }

2dd553978f6d1970ed59eadb38944249721ad422
paurbano <paurbano@gmail.com>
2020-05-11 23:45:20 +0000

    Other example Inherits

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const SquareFather = require('./5-square');
+
+module.exports = class Square extends SquareFather {
+  charPrint (c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        console.log('C'.repeat(this.width));
+      }
+    }
+  }
+};

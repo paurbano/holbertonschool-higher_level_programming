880d1f8dd9d4bb921119f4fffa97ebaddfe8d625
paurbano <paurbano@gmail.com>
2020-05-12 19:53:03 +0000

    Read file and printits content

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+var fs = require('fs');
+fs.readFile(process.argv[2], function (err, data) {
+    if (err) {
+      return console.error(err);
+    }
+    console.log(data.toString());
+});

27aed4a1e64db8c7180e71945e8934c6aad991d1
elisa2105 <1205@holbertonschool.com>
2020-05-14 06:52:51 -0500

    fetches and lists content from a JSON file

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,7 @@
+// Jquery
+let url = "https://swapi-api.hbtn.io/api/films/?format=json";
+$.getJSON(url, function(response) {
+  $.each(response.results, function(i, item) {
+    $("UL#list_movies").append("<li>" + item.title + "</li>");
+  });
+})

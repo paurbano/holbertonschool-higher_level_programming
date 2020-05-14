53157846c5b8a766269e80191377dc501949ad1b
paurbano <paurbano@gmail.com>
2020-05-14 16:42:10 +0000

    jquery getJSON() Get JSON data using an AJAX request, and output the result

--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -1,5 +1,5 @@
 // Jquery
-let url = "https://fourtonfish.com/hellosalut/?lang=fr";
-$.getJSON(url, function(response) {
-  $("DIV#hello").text(response.hello);
+const url = 'https://fourtonfish.com/hellosalut/?lang=fr';
+$.getJSON(url, function (response) {
+  $('DIV#hello').text(response.hello);
 });
